3ad8d0305c9733c0f2990603eb2cae30f587c388
Soledad <soledadescobar@yahoo.com>
2019-02-25 15:09:47 -0300

    Req

--- opcapp/admin.py
+++ opcapp/admin.py
@@ -2,5 +2,9 @@
 from django.contrib import admin
 
 from .models import Variablesmacroeconomicas
+from .models import Diashabiles
+from .models import Serieipc
 
 admin.site.register(Variablesmacroeconomicas)
+admin.site.register(Diashabiles)
+admin.site.register(Serieipc)
